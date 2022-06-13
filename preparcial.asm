9fc1ab07f26121470fe304d0a30c062b338083c9
ramiro213124 <79549922+ramiro213124@users.noreply.github.com>
2022-06-13 00:54:48 -0300

    Add files via upload

---
+++ preparcial.asm
@@ -0,0 +1,10 @@
+/*
+Ejercicio 1 ⋆
+Escriba un programa en  ́unicamente en Assembler que tome un vector de 16 enteros de 32 bits y los sume.
+Tip 1 Utilice una etiqueta en la seccion data para crear el vector.
+Tip 2 Revise los defines vistos en la primer clase practica (DB, DW, DQ).
+¿Cual es el apropiado para un vector de enteros de 32 bits?
+*/
+
+section .data
+ 
